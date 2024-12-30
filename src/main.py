2808615502a7d0e4297001cb87db9dbb84f9d8b8
tetaud-sebastien <tetaud.sebastien@gmail.com>
2024-12-30 19:01:06 +0000

    test bad code linter

--- src/main.py
+++ src/main.py
@@ -1,13 +1,12 @@
 import os
 import time
-
 import ccxt
 from dotenv import load_dotenv
 
 load_dotenv()
 API_KEY = os.getenv('BITGET_API_KEY')
-SECRET_KEY = os.getenv('BITGET_SECRET_KEY')
-PASSWORD = os.getenv('BITGET_PASSWORD')
+SECRET_KEY = os.getenv('BITGET_SECRET_KEY' )
+PASSWORD = os.getenv('BITGET_PASSWORD'  )
 
 
 # Initialize the Bitget exchange
@@ -19,7 +18,7 @@ bitget = ccxt.bitget({
 
 while True:
 
-    order_book = bitget.fetch_order_book('BTC/USDT', limit=1)
+    order_book = bitget.fetch_order_book('BTC/USDT', limit=1  )
     print(order_book['bids'])
     print(order_book['asks'])
     print("##")
