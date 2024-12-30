8f5ffaf734dd6665f4489b00e8eedd0a86bcee07
tetaud-sebastien <tetaud.sebastien@gmail.com>
2024-12-30 19:20:39 +0000

    correct code super-linter

--- src/main.py
+++ src/main.py
@@ -5,22 +5,22 @@ import ccxt
 from dotenv import load_dotenv
 
 load_dotenv()
-API_KEY = os.getenv('BITGET_API_KEY')
-SECRET_KEY = os.getenv('BITGET_SECRET_KEY')
-PASSWORD = os.getenv('BITGET_PASSWORD')
+API_KEY = os.getenv("BITGET_API_KEY")
+SECRET_KEY = os.getenv("BITGET_SECRET_KEY")
+PASSWORD = os.getenv("BITGET_PASSWORD")
 
 
 # Initialize the Bitget exchange
 bitget = ccxt.bitget({
-        'apiKey': API_KEY,
-        'secret': SECRET_KEY,
-        'password': PASSWORD,
+        "apiKey": API_KEY,
+        "secret": SECRET_KEY,
+        "password": PASSWORD,
     })
 
 while True:
 
-    order_book = bitget.fetch_order_book('BTC/USDT', limit=1)
-    print(order_book['bids'])
-    print(order_book['asks'])
+    order_book = bitget.fetch_order_book("BTC/USDT", limit=1)
+    print(order_book["bids"])
+    print(order_book["asks"])
     print("##")
     time.sleep(1)
