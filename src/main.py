60cb3a098925a47279ec5a5cf8d5465fb6fc7781
tetaud-sebastien <tetaud.sebastien@gmail.com>
2024-12-31 22:03:41 +0000

    add main code to get data

--- src/main.py
+++ src/main.py
@@ -1,28 +1,11 @@
-import os
-import time
+from utils.general import OHLCVScraper
 
-import ccxt
-from dotenv import load_dotenv
-
-load_dotenv()
-API_KEY = os.getenv("BITGET_API_KEY")
-SECRET_KEY = os.getenv("BITGET_SECRET_KEY")
-PASSWORD = os.getenv("BITGET_PASSWORD")
-
-
-# Initialize the Bitget exchange
-bitget = ccxt.bitget(
-    {
-        "apiKey": API_KEY,
-        "secret": SECRET_KEY,
-        "password": PASSWORD,
-    }
-)
-
-while True:
-
-    order_book = bitget.fetch_order_book("BTC/USDT", limit=1)
-    print(order_book["bids"])
-    print(order_book["asks"])
-    print("##")
-    time.sleep(1)
+if __name__ == "__main__":
+    scraper = OHLCVScraper(exchange_id="bitget")
+    scraper.scrape_candles_to_csv(
+        symbol="ETH/USDT",
+        timeframe="1d",
+        start_date_str="2021-01-01T00:00:00",
+        end_date_str="2024-01-01T00:00:00",
+        limit=100
+        )       
